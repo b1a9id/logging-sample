23f9726c023bec61ade9537907e32b6bd976972a
Ryosuke Uchitate <hello.ryosuke.u@gmail.com>
2021-03-19 09:54:55 +0900

    Initial

---
+++ src/test/java/com/b1a9idps/loggingsample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1a9idps.loggingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
